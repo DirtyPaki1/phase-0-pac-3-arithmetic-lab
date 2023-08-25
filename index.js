905c0a8530755c5e8690fee92f73539f98ad550e
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-24 21:41:44 -0400

    Function Parameters Completed

---
+++ index.js
@@ -0,0 +1,20 @@
+function makeInt(string){
+  string = parseInt("10");
+  return string
+  
+
+
+}
+
+console.log(makeInt())
+
+
+
+function preserveDecimal(string){
+  string = parseFloat('123.45677668867997')
+  return string
+}
+
+console.log(preserveDecimal())
+
+
