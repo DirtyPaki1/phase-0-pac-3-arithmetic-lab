19cd1950683ba76ec9697cd86c9de0b88d811713
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-26 14:22:00 -0400

    Parameter Completed

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-function makeInt(string){
-  string = parseInt("10");
-  return string
+function makeInt(n){
+  
+  return parseInt(n,10)
+  
+ 
+
+  
+
   
 
 
 }
-
-console.log(makeInt())
-
-
-
-function preserveDecimal(string){
-  string = parseFloat('123.45677668867997')
-  return string
+function add(a,b){
+  return a + b
 }
 
-console.log(preserveDecimal())
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+
+ function increment(n){
+  n++
+  return n
+ }
+function decrement(n){
+  n--
+  return n
+}
+
+
+
+
+
+function preserveDecimal(n){
+  return parseFloat(n)
+ 
+
+}
+
+
 
 
